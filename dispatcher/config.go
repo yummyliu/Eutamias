6a1459d0e7b6c5db151edade9bb50c9fcd7dfca8
yummyliu <liuyangming_1992@163.com>
2017-08-24 18:43:07 +0800

    dis iiiiint

--- dispatcher/config.go
+++ dispatcher/config.go
@@ -11,56 +11,70 @@ type Config struct {
 	Nips        []string
 	Nports      []string
 	cport       int
+	nport		int
 }
 
 type ConfigError struct {
 	info string
 }
 
-func (c *ConfigError) Error() string{
+func (c *ConfigError) Error() string {
 	return fmt.Sprintf("%s", c.info)
 }
 
 func (con *Config) Read(cfgPath string) error {
 	cfg, err := ini.InsensitiveLoad(cfgPath)
 	if err != nil {
-		return nil
+		return err
 	}
 
 	serve_sec, err := cfg.GetSection("servers")
 	if err != nil {
-		return nil
+		return err
 	}
 
 	_nports, err := serve_sec.GetKey("nports")
 	if err != nil {
-		return nil
+		return err
 	}
 	nports := strings.Split(_nports.String(), ",")
 
 	_nips, err := serve_sec.GetKey("nips")
 	if err != nil {
-		return nil
+		return err
 	}
 	nips := strings.Split(_nips.String(), ",")
 	if len(nports) != len(nips) {
 		return &ConfigError{
-			info : "nports != nips",
+			info: "nports != nips",
 		}
 	}
 
 	lf, err := serve_sec.GetKey("logfilepath")
 	if err != nil {
-		return nil
+		return err
 	}
 	cport, err := serve_sec.GetKey("clientport")
 	if err != nil {
-		return nil
+		return err
 	}
-	fmt.Println("%s",lf.String())
+	ccport, err := cport.Int()
+	if err != nil {
+		return err
+	}
+	nport, err := serve_sec.GetKey("NserverListenPort")
+	if err != nil {
+		return err
+	}
+	nnport, err := nport.Int()
+	if err != nil {
+		return err
+	}
+
 	con.LogFilePath = lf.String()
 	con.Nips = nips
 	con.Nports = nports
-	con.cport, err = cport.Int()
-	return err
+	con.cport = ccport
+	con.nport = nnport
+	return nil
 }
--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -9,45 +9,27 @@ import (
 	"strconv"
 )
 
-var (
-	clients = make(map[ClientChan]bool)
-	entering = make(chan ClientChan)
-	leaving = make(chan ClientChan)
-	messages = make(chan string)
-
-	config Config
-	nServerInfos []Nserver
-	totalOnlineUser uint64
-	log *logging.Logger
-)
-
 type Nserver struct {
 	Ip       string
-	Ip2      string // maybe has two ip, dianxin or wangtong
 	Port     int
 	MaxConn  int
 	CurConn  int
 	Hostname string
 }
+
 type ClientChan chan<- string
 
+var (
+	clients  = make(map[ClientChan]bool)
+	entering = make(chan ClientChan)
+	leaving  = make(chan ClientChan)
+	messages = make(chan string)
 
-
-func broadcaster() {
-	for {
-		select {
-		case msg := <- messages:
-			for cli := range clients {
-				cli <- msg
-			}
-		case cli := <-entering:
-			clients[cli] = true
-		case cli := <- leaving:
-			delete(clients, cli)
-			close(cli)
-		}
-	}
-}
+	config          Config
+	nServerInfos    []Nserver
+	totalOnlineUser uint64
+	log             *logging.Logger
+)
 
 func init_log(logFilePath string) error {
 	log = logging.MustGetLogger("main")
@@ -64,84 +46,82 @@ func init_log(logFilePath string) error {
 		var backendFormatter = logging.NewBackendFormatter(backend, format)
 		var backendLeveled = logging.AddModuleLevel(backendFormatter)
 		backendLeveled.SetLevel(logging.DEBUG, "main")
-
 		logging.SetBackend(backendFormatter)
 	} else {
 		var backend = logging.NewLogBackend(os.Stderr, "D:", 0)
 		var backendFormatter = logging.NewBackendFormatter(backend, format)
 		var backendLeveled = logging.AddModuleLevel(backendFormatter)
 		backendLeveled.SetLevel(logging.DEBUG, "main")
-
 		logging.SetBackend(backendFormatter)
 	}
-
 	return nil
 }
 
-func SyncNinfo() {
-	for {
-
-	}
-}
-
-func clientWriter(conn net.Conn, ch <-chan string) {
+func sendOut(conn net.Conn, ch <-chan string) {
 	for msg := range ch {
 		fmt.Fprintln(conn, msg)
 	}
 }
 
-func handle_client(con net.Conn) {
+func handleClient(con net.Conn) {
 	log.Info("get one client")
-	ch := make(chan string)
-	go clientWriter(con, ch)
+	out := make(chan string)
+	go sendOut(con, out)
 
 	who := con.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- ch
+	out <- "You are " + who
 
 	input := bufio.NewScanner(con)
 	for input.Scan() {
-		messages <- who + ": " + input.Text()
+		out <- "have got you msg: " + input.Text()
 	}
 
-	leaving <- ch
-	messages <- who + " has left"
 	con.Close()
 }
+func handleNserver(con net.Conn) {
+	log.Info("get one Nserver")
+	out := make(chan string)
+	go sendOut(con, out)
 
-func main() {
-//	err := init_log(config.LogFilePath)
-	err := init_log("")
-	if err != nil {
-		fmt.Printf("%s", err.Error())
-		return
+	who := con.RemoteAddr().String()
+	out <- "You are " + who
+
+	input := bufio.NewScanner(con)
+	for input.Scan() {
+		out <- "have got you msg: " + input.Text()
 	}
 
-	// read conf
-	config.Read("dispatcher.ini")
-	nServerInfos = make([]Nserver, len(config.Nips))
-	for i, n := range config.Nips {
-		nServerInfos[i].Ip = n
-		nServerInfos[i].Port, err = strconv.Atoi(config.Nports[i])
-		if err != nil {
-			fmt.Println("port int ?")
-			return
-		}
-		log.Infof("config: %s:%s:%s", config.Nips, config.Nports,config.cport)
-	}
-
-	// Listening
-	listener, err := net.Listen("tcp", "0.0.0.0:8000")
+	con.Close()
+}
+func Listen(addr string, f func(net.Conn)) {
+	log.Infof("Listen on; %s", addr)
+	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Info(err)
 			continue
 		}
-		go handle_client(conn)
+		go f(conn)
 	}
+}
+
+func main() {
+	//	err := init_log(config.LogFilePath)
+	err := init_log("")
+	if err != nil {
+		log.Info("%s",err)
+		return
+	}
+
+	// read conf
+	config.Read("dispatcher.ini")
+
+	// Listening
+	go Listen("0.0.0.0:"+strconv.Itoa(config.cport), handleClient)
+	Listen("0.0.0.0:"+strconv.Itoa(config.nport), handleNserver)
 }
